6a91ddfa8b81ecc3a65f82c0cab67bf639f11c00
Ho-mmd <ws000617@gmail.com>
2023-02-13 19:07:16 +0900

    just

---
+++ Python/Lv.1/1.py
@@ -0,0 +1,14 @@
+def solution(d, budget):
+    answer = 0
+    d.sort();
+    
+    for i in d:
+        budget -= i;
+        answer += 1;
+        if(budget == 0):
+            break;
+        elif(budget < 0):
+            answer -= 1;
+            break;
+    
+    return answer
